b5cf48b78141adb32ca385a56b0bd05f9b01ec06
elanini <elanini@ucsc.edu>
2016-02-19 10:05:12 -0800

    Add checking and comments

--- PreviewNoSidebar/PreviewNoSidebar.m
+++ PreviewNoSidebar/PreviewNoSidebar.m
@@ -17,21 +17,28 @@ typedef void (^CDUnknownBlockType)(void);
 @end
 
 ZKSwizzleInterface($_Replacement, PVWindowController, NSObject);
-
 @implementation $_Replacement
+//we need this here so we can call it
+//would use performSelector:withObject:withObject but BOOL isnt an object
+//alternative would be NSInvocation
 - (void)closeSidebarWithAnimation:(BOOL)arg1 completionHandler:(CDUnknownBlockType)arg2
 {
     ZKOrig(void, arg2, arg2);
 }
 
-
+//openSidebarWithAnimation is called from windowDidLoad
+//I just undo what it did already, closing the sidebar if there is only one page open in
+//the window and it is a PDF
 - (void)windowDidLoad
 {
     ZKOrig(void);
-    NSArray *array = [ZKHookIvar(self, NSArrayController*, "_containersArrayController") arrangedObjects];
-    if (    [array count] == 1
-        && [[array  firstObject] class] == NSClassFromString(@"PVPDFPageContainer")) {
-        [self closeSidebarWithAnimation:NO completionHandler:nil];
+    id arrayIvar = ZKHookIvar(self, id, "_containersArrayController");
+    if ([arrayIvar class] == NSClassFromString(@"NSArrayController")) {;
+        NSArray *array = [(NSArrayController*)arrayIvar arrangedObjects];
+        if ([array count] == 1 &&
+            [[array  firstObject] class] == NSClassFromString(@"PVPDFPageContainer")) {
+            [self closeSidebarWithAnimation:NO completionHandler:nil];
+        }
     }
 }
 
